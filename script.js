8744a6206252d8130a5c625f85a11f396c92d740
Daniel Ximenes <93453046+Denionline@users.noreply.github.com>
2024-10-05 18:26:24 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const date_timeElement = document.querySelector('.date_time');
+
+const timeElement = date_timeElement.querySelector('.time');
+const hourElement = timeElement.querySelector('.hour');
+const minutesElement = timeElement.querySelector('.minutes');
+
+const getCurrentTime = () => {
+    const currentTime = new Date();
+
+    const hour = currentTime.getHours();
+    const minutes = currentTime.getMinutes();
+
+    console.log(`${hour}:${minutes}`)
+}
+
+const btn_mark = document.querySelector('btn_mark');
+
+btn_mark.addEventListener('click', () => {
+    getCurrentTime();
+})
